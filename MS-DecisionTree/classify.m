c367b82c698f16a85e02d6246087a9195c46156d
dipayan90 <dipayan90@gmail.com>
2014-08-07 17:51:39 -0400

    <removed duplicates>

--- MS-DecisionTree/classify.m
+++
@@ -1,8 +0,0 @@
-function [ output_args ] = classify(tree,X)
-%Based on the created tree this function is going to predict classes. After prediction it is going to provide the accuracy of these predictions.
-[m,c]=size(X);
-
-
-
-end
-
